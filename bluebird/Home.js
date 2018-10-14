f9c71c56e36bd587bb20f17f982cd43eb251ab49
sogangCSmaster <cs20131575@u.sogang.ac.kr>
2018-10-14 14:52:21 +0900

    move to right directory

--- bluebird/Home.js
+++ bluebird/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Scene, Router, Modal, Actions } from 'react-native-router-flux';
 
 export default class App extends React.Component {
     render() {
